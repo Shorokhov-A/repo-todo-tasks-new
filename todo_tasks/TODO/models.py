90419d25c6c022674bce6208ac30970c2755731c
Shorockhov-A <andr2409@bk.ru>
2021-12-13 21:12:20 +0300

    added Project model

--- todo_tasks/TODO/models.py
+++ todo_tasks/TODO/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    name = models.CharField(max_length=64)
+    link = models.CharField(max_length=256, blank=True, null=True)
+    users = models.ManyToManyField(User)
