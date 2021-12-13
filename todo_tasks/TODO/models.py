3138819771179d5d9cb81f536a6dfa1672bcb990
Shorockhov-A <andr2409@bk.ru>
2021-12-13 21:29:29 +0300

    added ToDo model

--- todo_tasks/TODO/models.py
+++ todo_tasks/TODO/models.py
@@ -6,3 +6,12 @@ class Project(models.Model):
     name = models.CharField(max_length=64)
     link = models.CharField(max_length=256, blank=True, null=True)
     users = models.ManyToManyField(User)
+
+
+class ToDo(models.Model):
+    project = models.ForeignKey(Project, models.PROTECT)
+    text = models.TextField()
+    created = models.DateTimeField(verbose_name='дата создания', auto_now_add=True)
+    updated = models.DateTimeField(verbose_name='дата обновления', auto_now=True)
+    user = models.ForeignKey(User, models.PROTECT)
+    is_active = models.BooleanField(default=True)
