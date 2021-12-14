ee5b291d1464ac525be797986cbb6b70067b296a
Shorockhov-A <andr2409@bk.ru>
2021-12-14 13:27:04 +0300

    added ToDoModelSerializer

--- todo_tasks/TODO/serializers.py
+++ todo_tasks/TODO/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer, StringRelatedField
 from TODO.models import Project, ToDo
+from users.serializers import UserModelSerializer
 
 
 class ProjectModelSerializer(ModelSerializer):
@@ -8,3 +9,11 @@ class ProjectModelSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = ('name', 'link', 'users')
+
+
+class ToDoModelSerializer(ModelSerializer):
+    user = UserModelSerializer()
+
+    class Meta:
+        model = ToDo
+        fields = ('text', 'created', 'updated', 'user', 'is_active')
