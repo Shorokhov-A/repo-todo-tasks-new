56ef6a36b13a5793235d90bc144521969f6c3384
Shorockhov-A <andr2409@bk.ru>
2021-12-14 18:46:22 +0300

    edited UserModelSerializer

--- todo_tasks/users/serializers.py
+++ todo_tasks/users/serializers.py
@@ -1,8 +1,16 @@
+from rest_framework.relations import HyperlinkedRelatedField
 from rest_framework.serializers import ModelSerializer, HyperlinkedModelSerializer
 from .models import User
 
 
-class UserModelSerializer(ModelSerializer):
+class UserModelSerializer(HyperlinkedModelSerializer):
+    user_permissions = HyperlinkedRelatedField(
+        view_name='permission-detail',
+        lookup_field='user_permissions',
+        many=True,
+        read_only=True,
+    )
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        # fields = ('username', 'first_name', 'last_name', 'email')
+        fields = '__all__'
