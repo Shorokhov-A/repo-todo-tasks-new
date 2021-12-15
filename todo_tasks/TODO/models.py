e8a06007111dbf7873f1d490a46bca729833205d
Shorockhov-A <andr2409@bk.ru>
2021-12-15 18:38:37 +0300

    edited Project model

--- todo_tasks/TODO/models.py
+++ todo_tasks/TODO/models.py
@@ -4,7 +4,7 @@ from users.models import User
 
 class Project(models.Model):
     name = models.CharField(max_length=64)
-    link = models.CharField(max_length=256, blank=True, null=True)
+    link = models.URLField(max_length=256, blank=True, null=True)
     users = models.ManyToManyField(User)
 
 
