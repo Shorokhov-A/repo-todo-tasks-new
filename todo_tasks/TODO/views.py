93c3a4faab248479d7640f043d1e21956cf66b88
Shorockhov-A <andr2409@bk.ru>
2021-12-15 13:21:43 +0300

    edited ProjectModelViewSet

--- todo_tasks/TODO/views.py
+++ todo_tasks/TODO/views.py
@@ -1,9 +1,11 @@
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from TODO.models import Project, ToDo
 from TODO.serializers import ProjectModelSerializer, ToDoModelSerializer
 
 
 class ProjectModelViewSet(ModelViewSet):
+    renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
 
