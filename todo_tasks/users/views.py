8c3680b759444a4e54182bb4afbf5d8dc5d7b765
Shorockhov-A <andr2409@bk.ru>
2021-12-16 12:57:46 +0300

    added camel case renderers to UserModelViewSet

--- todo_tasks/users/views.py
+++ todo_tasks/users/views.py
@@ -1,10 +1,11 @@
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
+from djangorestframework_camel_case.render import CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
 from users.models import User
 from users.serializers import UserModelSerializer
 
 
 class UserModelViewSet(ModelViewSet):
-    renderer_classes = (JSONRenderer, BrowsableAPIRenderer)
+    renderer_classes = (CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer)
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
