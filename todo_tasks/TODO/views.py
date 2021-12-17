374b6e3262bb2a33b799f4f69d151dfcab535986
Shorockhov-A <andr2409@bk.ru>
2021-12-17 12:04:47 +0300

    added ProjectLimitOffsetPagination and ProjectFilter to ProjectModelViewSet

--- todo_tasks/TODO/views.py
+++ todo_tasks/TODO/views.py
@@ -1,14 +1,22 @@
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework.viewsets import ModelViewSet
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
+from rest_framework.pagination import LimitOffsetPagination
 from TODO.models import Project, ToDo
+from TODO.filters import ProjectFilter
 from TODO.serializers import ProjectModelSerializer, ToDoModelSerializer
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     renderer_classes = (CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer)
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class ToDoModelViewSet(ModelViewSet):
