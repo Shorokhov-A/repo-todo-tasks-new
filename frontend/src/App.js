9611c5f3ce9e5fbba6f32ad47375caf175e5a3a7
Shorockhov-A <andr2409@bk.ru>
2021-12-22 10:48:22 +0300

    modified User component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,26 +4,38 @@ import './App.css';
 import UserList from "./components/User.js";
 import MenuList from "./components/Menu.js";
 import Footer from "./components/Footer.js";
+import ProjectList from "./components/Projects.js";
 import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      'users': []
+        'users': [],
+        // 'projects': [],
     }
   }
 
   componentDidMount() {
     axios.get('http://127.0.0.1:8000/api/users')
         .then(response => {
-          const users = response.data
+          const users = response.data.results
           this.setState(
               {
                 'users': users
               }
               )
-            }).catch(error => console.log(error))
+        }).catch(error => console.log(error))
+
+    // axios.get('http://127.0.0.1:8000/api/projects')
+    //     .then(response => {
+    //       const projects = response.data.result
+    //       this.setState(
+    //           {
+    //             'projects': projects
+    //           }
+    //           )
+    //     }).catch(error => console.log(error))
   }
 
   render() {
@@ -31,6 +43,7 @@ class App extends React.Component {
         <div>
             <MenuList />
             <UserList users={this.state.users} />
+            {/*<ProjectList projects={this.state.projects} />*/}
             <Footer />
         </div>
     )
