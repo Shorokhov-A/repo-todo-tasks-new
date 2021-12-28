07db16691dd9f698ea19d7a86a0beef7a334f654
Shorockhov-A <andr2409@bk.ru>
2021-12-28 17:47:29 +0300

    edited UserModelSerializer

--- todo_tasks/users/serializers.py
+++ todo_tasks/users/serializers.py
@@ -4,20 +4,20 @@ from .models import User
 
 
 class UserModelSerializer(HyperlinkedModelSerializer):
-    user_permissions = HyperlinkedRelatedField(
-        view_name='permission-detail',
-        lookup_field='user_permissions',
-        many=True,
-        read_only=True,
-    )
-    groups = HyperlinkedRelatedField(
-        view_name='group-detail',
-        lookup_field='groups',
-        many=True,
-        read_only=True,
-    )
+    # user_permissions = HyperlinkedRelatedField(
+    #     view_name='permission-detail',
+    #     lookup_field='user_permissions',
+    #     many=True,
+    #     read_only=True,
+    # )
+    # groups = HyperlinkedRelatedField(
+    #     view_name='group-detail',
+    #     lookup_field='groups',
+    #     many=True,
+    #     read_only=True,
+    # )
 
     class Meta:
         model = User
-        # fields = ('username', 'first_name', 'last_name', 'email')
-        exclude = ('password',)
+        fields = ('url', 'username', 'first_name', 'last_name', 'email')
+        # exclude = ('password',)
