ae44e4480fa907fb531816ddc0982f5323113923
Shorockhov-A <andr2409@bk.ru>
2021-12-29 12:51:43 +0300

    added UserObtainAuthToken view

--- todo_tasks/todo_tasks/urls.py
+++ todo_tasks/todo_tasks/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
-from users.views import UserCustomViewSet
+from users.views import UserCustomViewSet, UserObtainAuthToken
 from TODO.views import ProjectModelViewSet, ToDoModelViewSet
 
 router = DefaultRouter()
@@ -30,7 +30,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token),
+    # path('api-token-auth/', views.obtain_auth_token),
+    path('api-token-auth/', UserObtainAuthToken.as_view()),
     path('api/jwt-token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/jwt-token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/jwt-token/verify/', TokenVerifyView.as_view(), name='token_verify'),
--- todo_tasks/users/views.py
+++ todo_tasks/users/views.py
@@ -1,4 +1,7 @@
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from djangorestframework_camel_case.render import CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer
 from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
@@ -10,3 +13,13 @@ class UserCustomViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, Ge
     renderer_classes = (CamelCaseJSONRenderer, CamelCaseBrowsableAPIRenderer)
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+
+class UserObtainAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key, 'user': f'{user.first_name} {user.last_name}'})
