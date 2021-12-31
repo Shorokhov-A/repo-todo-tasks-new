795583c6361a61f182ee2bedd5aa5cd3102ec39e
Shorockhov-A <andr2409@bk.ru>
2021-12-31 10:11:17 +0300

    added test_create_guest for ProjectModelViewSet

--- todo_tasks/TODO/tests.py
+++ todo_tasks/TODO/tests.py
@@ -12,3 +12,14 @@ class TestProjectModelViewSet(TestCase):
         view = ProjectModelViewSet.as_view({'get': 'list'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_guest(self):
+        factory = APIRequestFactory()
+        request = factory.post(
+            '/api/projects/',
+            {'name': 'Test project', 'link': 'https://test_project.com'},
+            format='json',
+        )
+        view = ProjectModelViewSet.as_view({'post': 'create'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
