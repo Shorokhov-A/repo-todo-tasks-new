9b2596df30df3660e5a16eea3f4794dbf319d912
Shorockhov-A <andr2409@bk.ru>
2021-12-31 10:10:06 +0300

    added test_get_list for ProjectModelViewSet

--- todo_tasks/TODO/tests.py
+++ todo_tasks/TODO/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
 
-# Create your tests here.
+from TODO.views import ProjectModelViewSet
+
+
+class TestProjectModelViewSet(TestCase):
+    def test_get_list(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/projects/')
+        view = ProjectModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
