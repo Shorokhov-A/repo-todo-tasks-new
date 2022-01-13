9df1cd34e9827ea174f87fcaed36a1618c5a12e4
Shorockhov-A <andr2409@bk.ru>
2022-01-13 14:06:30 +0300

    added documentation to API

--- todo_tasks/todo_tasks/urls.py
+++ todo_tasks/todo_tasks/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from users.views import UserCustomViewSet, UserObtainAuthToken
 from TODO.views import ProjectModelViewSet, ToDoModelViewSet
 
@@ -25,6 +30,18 @@ router = DefaultRouter()
 router.register('users', UserCustomViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', ToDoModelViewSet)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='todo_tasks',
+        default_version='1.0',
+        description='Documentation to out project',
+        contact=openapi.Contact(email='admin@admin.local'),
+        license=openapi.License(name='MIT License'),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,4 +52,7 @@ urlpatterns = [
     path('api/jwt-token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/jwt-token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/jwt-token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
