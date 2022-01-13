a2d9a36c6579dd335936348208e4ea645fd477c2
Shorockhov-A <andr2409@bk.ru>
2022-01-13 12:45:33 +0300

    added API v2.0

--- todo_tasks/todo_tasks/settings.py
+++ todo_tasks/todo_tasks/settings.py
@@ -149,4 +149,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.AcceptHeaderVersioning',
 }
