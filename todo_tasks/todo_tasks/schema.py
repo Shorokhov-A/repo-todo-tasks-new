6c742a027c3fb749cd4addec4c4b817a78ee9c06
Shorockhov-A <andr2409@bk.ru>
2022-01-17 21:43:01 +0300

    added UserType

--- todo_tasks/todo_tasks/schema.py
+++ todo_tasks/todo_tasks/schema.py
@@ -16,15 +16,25 @@ class ProjectType(DjangoObjectType):
         fields = '__all__'
 
 
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
 class Query(graphene.ObjectType):
     all_tasks = graphene.List(ToDoType)
     all_projects = graphene.List(ProjectType)
+    all_users = graphene.List(UserType)
 
     def resolve_all_tasks(root, info):
         return ToDo.objects.all()
 
     def resolve_all_projects(root, info):
         return Project.objects.all()
+
+    def resolve_all_users(root, info):
+        return  User.objects.all()
 
 
 schema = graphene.Schema(query=Query)
